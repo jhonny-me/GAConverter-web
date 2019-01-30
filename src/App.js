9d2dcc271e01ee69e7fe188940581796527d2d74
Johnny Gu <johnny@wiredcraft.com>
2019-01-30 17:43:36 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios, { post } from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      file: null
+    }
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state.file)
+    this.fileUpload(this.state.file).then((res) => {
+      console.log(res)
+    })
+  }
+  onChange = (e) => {
+    this.setState({file: e.target.files[0]})
+  }
+
+  fileUpload = (file) => {
+    const url = 'http://localhost:8000/file-upload'
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('fileName', file.name)
+    const config = {
+      hearders: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+    return post(url, formData, config)
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,6 +53,13 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <body>
+          <form onSubmit={this.onFormSubmit}>
+            <h1>File Upload</h1>
+            <input type="file" onChange={this.onChange} />
+            <button type="submit">Upload</button>
+          </form>
+        </body>
       </div>
     );
   }
